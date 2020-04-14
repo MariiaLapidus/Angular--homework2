dbf2b8419ed01c1b5f7cc9838ddc06a13ca574be
mmvli <mmvlivo@gmail.com>
2020-04-14 21:41:50 +0300

    done

---
+++ src/app/services/comment.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {CommentModel} from '../models/CommentModel';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentService {
+  API = 'https://jsonplaceholder.typicode.com/';
+  constructor(private httpClient: HttpClient) { }
+  getAllData(): Observable<CommentModel[]>{
+    return this.httpClient.get<CommentModel[]>(this.API + 'comments');
+  }
+}
